1f027c71509760f164c6995cf1035356484cda4f
Young-Oxide <yyf2058@gmail.com>
2024-10-15 15:11:54 +0800

    Create Demo.cpp

---
+++ Demo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Table.h"
+
+using namespace std;
+
+double calcR(double* V,int VarNum){
+	return V[0]/V[1];
+}
+
+int main(){
+	Table t1(2,3);
+	
+	t1.add_Var("R=\\frac{U}{I}","\\Omega",calcR);
+	
+	// freopen("data.markdown","w",stdout);
+	t1.all_Original_Data();
+	t1.calc_All();
+	// fclose(stdout);
+	// freopen("CON","w",stdout);
+	cout<<"Output finished!\n";
+	return 0;
+}
