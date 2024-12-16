9a91bd74114cf3dcdad140dcbd076515c7eb7065
Young-Oxide <yyf2058@gmail.com>
2024-12-16 17:35:10 +0800

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -11,7 +11,8 @@ int main(){
 	Table t1(2,3);
 	
 	t1.add_Var("R=\\frac{U}{I}","\\Omega",calcR);
-	
+
+	//If you want to output in files, do not comment the code below
 	// freopen("data.markdown","w",stdout);
 	t1.all_Original_Data();
 	t1.calc_All();
